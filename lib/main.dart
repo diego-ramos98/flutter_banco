b01043cbd8e2b525d314d69334bcfe0e6252915e
diego-ramos98 <diego.ramos.fortunato@outlook.com>
2024-12-29 23:25:00 -0300

    :tada: feat inicial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_banco/ui/login_screen.dart';
+
+void main() {
+  runApp(const BancoDouroApp());
+}
+
+
+class BancoDouroApp extends StatelessWidget{
+  const BancoDouroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
+
+
